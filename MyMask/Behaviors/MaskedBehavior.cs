0127e4ec590c4c68088277be609dbb8ad6eb2f8c
unknown <josan.agzb@gmail.com>
2021-08-03 04:13:48 -0500

    First commit

---
+++ MyMask/Behaviors/MaskedBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace MyMask.Behaviors
+{
+    public class MaskedBehavior : Behavior<Entry>
+    {
+        private string _mask = "";
+
+        public string Mask
+        {
+            get => _mask;
+            set
+            {
+                _mask = value;
+            }
+        }
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            var entry = sender as Entry;
+            var text = Regex.Replace(entry.Text, "[^0-9]", "");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Mask[i] != 'X')
+                {
+                    text = text.Insert(i, Mask[i].ToString());
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(Mask))
+            {
+                // Adding the MaxLength
+                if (text.Length == Mask.Length)
+                    entry.MaxLength = Mask.Length;
+            }
+
+            entry.Text = text;
+            entry.Placeholder = Mask;
+        }
+    }
+}
---
+++ MyMask/Converters/CardMaskedConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace MyMask.Converters
+{
+    public class CardMaskedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int textLength = (int) value;
+            string maskPrompt = string.Empty;
+
+            #region Adjust blank spaces
+            if (textLength > 4 && textLength < 10)
+                textLength--;
+            else if (textLength > 9 && textLength < 15)
+                textLength -= 2;
+            else if (textLength > 14 && textLength < 20)
+                textLength -= 3;
+            else if (textLength > 19)
+                textLength -= 4;
+            #endregion
+
+            maskPrompt = maskPrompt.PadRight(textLength, '	');
+
+            if (textLength <= 11)
+                maskPrompt = maskPrompt.PadRight(11, '_');
+            else if (textLength >= 12 && textLength <= 16)
+                maskPrompt = maskPrompt.PadRight(16, '_');
+            else if (textLength >= 17)
+                maskPrompt = maskPrompt.PadRight(18, '_');
+
+            for (int i = 4; i < maskPrompt.Length; i = i + 5)
+                maskPrompt = maskPrompt.Insert(i, " ");
+
+            return maskPrompt;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
